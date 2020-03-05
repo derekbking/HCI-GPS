37d9f623cf903faafc45a324e59a0537ab3ef49f
Derek King <derekbking@gmail.com>
2020-03-05 16:22:59 -0600

    UI Improvements

--- lib/menu.dart
+++ lib/menu.dart
@@ -15,50 +15,63 @@ class _MainMenuState extends State<MainMenu> {
     return Scaffold(
         backgroundColor: Colors.blueGrey[900],
         body: Center(
-          child: ConstrainedBox(
-            constraints: BoxConstraints.tightForFinite(width: 600),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Material(
-                    borderRadius: BorderRadius.circular(24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Select a Destination",
+                      style: GoogleFonts.montserrat(
+                          textStyle:
+                              TextStyle(color: Colors.white, fontSize: 60))),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: Text("or",
+                        style: GoogleFonts.montserrat(
+                            textStyle:
+                                TextStyle(color: Colors.white, fontSize: 60, fontWeight: FontWeight.w100))),
+                  ),
+                  Material(
+                    borderRadius: BorderRadius.circular(12.0),
                     color: Colors.white,
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          icon: Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: Icon(Icons.search),
-                          ),
-                          hintText: "Search a destination"),
-                      style: GoogleFonts.openSans(
-                          textStyle: TextStyle(fontSize: 20)),
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints.tightForFinite(width: 200),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            icon: Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Icon(Icons.search),
+                            ),
+                            hintText: "Search"),
+                        style: GoogleFonts.openSans(
+                            textStyle: TextStyle(fontSize: 20)),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 70),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    buildButton(Icons.restaurant),
-                    buildButton(Icons.local_gas_station),
-                    buildButton(Icons.local_hospital)
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    buildButton(Icons.local_hotel),
-                    buildButton(Icons.local_movies),
-                    buildButton(Icons.local_parking)
-                  ],
-                )
-              ],
-            ),
+                ],
+              ),
+              SizedBox(height: 35),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  buildButton(Icons.restaurant),
+                  buildButton(Icons.local_gas_station),
+                  buildButton(Icons.local_hospital)
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  buildButton(Icons.local_hotel),
+                  buildButton(Icons.local_movies),
+                  buildButton(Icons.local_parking)
+                ],
+              ),
+            ],
           ),
         ));
   }
